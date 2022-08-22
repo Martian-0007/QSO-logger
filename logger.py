26ea69952f562c7d67828bc1f7ec71399626c157
Martian-0007 <81148675+Martian-0007@users.noreply.github.com>
2022-08-22 14:22:01 +0200

    Created loop and made the code look pretier

--- logger.py
+++ logger.py
@@ -1,35 +1,47 @@
 import logfunctions as fx
 import json
 
-CALL = input("Callsing:\n")
-CALL = CALL.upper()
-
 
 try:
-    with open("db/"+CALL+".json", "r", encoding='utf-8') as fl:
-        content = fl.read()
-        data = json.loads(content)
-        print("\n"+CALL+":")
-        data["QSO count"] = data["QSO count"] + 1
-        print(data)
-    
-    with open("db/"+CALL+".json", "w", encoding='utf-8') as fl:
-        data = {
-            "QSO count" : data["QSO count"],
-            "Last seen" : fx.CurTime()
-        }
-        datawr = json.dumps(data)
-        print(datawr)
-        print(datawr, file = fl)
-        
+   
+   
+    while True:
+       
+       
+        try:
+            CALL = input("Callsing:\n")
+            CALL = CALL.upper()
+            
 
-except FileNotFoundError:
-    with open("db/"+CALL+".json", "a", encoding='utf-8') as fl:
-        content = {
-            "QSO count" : 1,
-            "Last seen" : fx.CurTime()
-        }
-        data = json.dumps(content)
-        print("\n"+CALL+":\n")
-        print(data)
-        print(data, file = fl)
+            with open("db/"+CALL+".json", "r", encoding='utf-8') as fl:
+                content = fl.read()
+                data = json.loads(content)
+                print("\n"+CALL+":")
+                data["QSO count"] = data["QSO count"] + 1
+                print(data)
+            
+            with open("db/"+CALL+".json", "w", encoding='utf-8') as fl:
+                data = {
+                    "QSO count" : data["QSO count"],
+                    "Last seen" : fx.CurTime()
+                }
+                datawr = json.dumps(data)
+                print(datawr)
+                print(datawr, file = fl)
+                
+
+        except FileNotFoundError:
+            with open("db/"+CALL+".json", "a", encoding='utf-8') as fl:
+                content = {
+                    "QSO count" : 1,
+                    "Last seen" : fx.CurTime()
+                }
+                data = json.dumps(content)
+                print("\n"+CALL+":\n")
+                print(data)
+                print(data, file = fl)
+
+
+except KeyboardInterrupt:
+    print("user ended")
+    quit()
